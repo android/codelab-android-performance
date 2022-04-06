92e0986376e8c57b351ba73419704d26f7bdc2cf
Tomáš Mlynarič <mlykotom@google.com>
2022-04-06 14:10:30 +0200

    chore: initial commit

---
+++ app/src/main/java/com/example/macrobenchmark_codelab/ui/JetsnackApp.kt
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.macrobenchmark_codelab.ui
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.material.SnackbarHost
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.testTagsAsResourceId
+import androidx.navigation.*
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.macrobenchmark_codelab.ui.components.JetsnackScaffold
+import com.example.macrobenchmark_codelab.ui.components.JetsnackSnackbar
+import com.example.macrobenchmark_codelab.ui.home.HomeSections
+import com.example.macrobenchmark_codelab.ui.home.JetsnackBottomBar
+import com.example.macrobenchmark_codelab.ui.home.addHomeGraph
+import com.example.macrobenchmark_codelab.ui.snackdetail.SnackDetail
+import com.example.macrobenchmark_codelab.ui.theme.JetsnackTheme
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun JetsnackMain() {
+    JetsnackTheme {
+        val appState = rememberJetsnackAppState()
+        JetsnackScaffold(
+            bottomBar = {
+                if (appState.shouldShowBottomBar) {
+                    JetsnackBottomBar(
+                        tabs = appState.bottomBarTabs,
+                        currentRoute = appState.currentRoute!!,
+                        navigateToRoute = appState::navigateToBottomBarRoute
+                    )
+                }
+            },
+            snackbarHost = {
+                SnackbarHost(
+                    hostState = it,
+                    modifier = Modifier.systemBarsPadding(),
+                    snackbar = { snackbarData -> JetsnackSnackbar(snackbarData) }
+                )
+            },
+            scaffoldState = appState.scaffoldState
+        ) { innerPaddingModifier ->
+            NavHost(
+                navController = appState.navController,
+                startDestination = MainDestinations.HOME_ROUTE,
+                modifier = Modifier.padding(innerPaddingModifier)
+            ) {
+                jetsnackNavGraph(
+                    onSnackSelected = appState::navigateToSnackDetail,
+                    upPress = appState::upPress
+                )
+            }
+        }
+    }
+}
+
+private fun NavGraphBuilder.jetsnackNavGraph(
+    onSnackSelected: (Long, NavBackStackEntry) -> Unit,
+    upPress: () -> Unit
+) {
+    navigation(
+        route = MainDestinations.HOME_ROUTE,
+        startDestination = HomeSections.FEED.route
+    ) {
+        addHomeGraph(onSnackSelected)
+    }
+    composable(
+        "${MainDestinations.SNACK_DETAIL_ROUTE}/{${MainDestinations.SNACK_ID_KEY}}",
+        arguments = listOf(navArgument(MainDestinations.SNACK_ID_KEY) { type = NavType.LongType })
+    ) { backStackEntry ->
+        val arguments = requireNotNull(backStackEntry.arguments)
+        val snackId = arguments.getLong(MainDestinations.SNACK_ID_KEY)
+        SnackDetail(snackId, upPress)
+    }
+}
