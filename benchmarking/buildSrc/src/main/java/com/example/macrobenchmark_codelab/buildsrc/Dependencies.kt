86148cff32cc9fa592e0090996ea1358ab7e141d
Tomáš Mlynarič <mlykotom@google.com>
2022-06-09 13:36:54 +0200

    chore AGP 7.3.0-beta02

--- benchmarking/buildSrc/src/main/java/com/example/macrobenchmark_codelab/buildsrc/Dependencies.kt
+++ benchmarking/buildSrc/src/main/java/com/example/macrobenchmark_codelab/buildsrc/Dependencies.kt
@@ -21,7 +21,7 @@ object Versions {
 }
 
 object Libs {
-    const val androidGradlePlugin = "com.android.tools.build:gradle:7.3.0-beta01"
+    const val androidGradlePlugin = "com.android.tools.build:gradle:7.3.0-beta02"
 
     object Accompanist {
         const val version = "0.24.8-beta"
